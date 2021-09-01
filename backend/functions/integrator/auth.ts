1350188c0ead7c00c0742e7d9f6bf025ed672a2c
saurabhjaswal <saurabh.jaswal@cyberoisystems.com>
2021-09-01 10:33:33 +0530

    update integrator

--- backend/functions/integrator/auth.ts
+++ backend/functions/integrator/auth.ts
@@ -1,5 +1,6 @@
 import {
   formatJSONResponseStatusBadRequest,
+  formatJSONResponseStatusCreated,
   formatJSONResponseStatusServerError,
 } from "@libs/apiGateway";
 import * as AWS from "aws-sdk";
@@ -27,51 +28,81 @@ export const auth = async (email, integrator_id) => {
       },
     ],
   };
-  try {
-    const createdIntegrator = await cognitoIdp
-      .adminCreateUser(params)
-      .promise();
-    console.log(
-      "Integrator created with userName :: ",
-      createdIntegrator.User.Username
-    );
-  } catch (err) {
-    if (err.code === "UsernameExistsException") {
-      console.log("Email already exists in Integrator Pool :: ", userPool);
-      return formatJSONResponseStatusBadRequest({
-        message: "Email already exists.",
-      });
-    } else {
-      console.log(
-        "Error while adding Integrator in Integrator Pool(adminCreateUser) :: ",
-        err
-      );
-      return formatJSONResponseStatusServerError({
-        message: "Error while adding Integrator in Integrator Pool",
-        err,
-      });
-    }
-  }
+  
+  return await cognitoIdp.adminCreateUser(params).promise()
+  .then(data => {
+    console.log("Email Added :: ", data);
 
-  try {
-    await cognitoIdp
-      .adminAddUserToGroup({
-        GroupName: "IntegratorGroup",
-        UserPoolId: userPool,
-        Username: email,
-      })
-      .promise();
-    console.log(
-      `Integrator with id ${integrator_id} is added to IntegratorGroup`
-    );
-  } catch (err) {
-    console.log(
-      "Error while adding Integrator in Integrator Pool (adminAddUserToGroup) :: ",
-      err
-    );
-    return formatJSONResponseStatusServerError({
-      message: "Error while adding Integrator in Integrator Pool",
-      err,
+    return cognitoIdp
+    .adminAddUserToGroup({
+      GroupName: "IntegratorGroup",
+      UserPoolId: userPool,
+      Username: email,
+    }).promise()
+    .then(data => {
+      console.log("Added Integrator group :: ", data);
+      return formatJSONResponseStatusCreated({
+        message: "Integrator added successfully"
+      })  
+    })
+    .catch(err => {
+      console.log("Unable to update group :: ", err);
+    })
+    
+  })
+  .catch(err => {
+    console.log("Unable to add Email ID :: ", err);
+    return formatJSONResponseStatusBadRequest({
+      message: "Email already exists"
     });
-  }
+  })
+
+
+  // try {
+  //   const createdIntegrator = await cognitoIdp
+  //     .adminCreateUser(params)
+  //     .promise();
+  //   console.log(
+  //     "Integrator created with userName :: ",
+  //     createdIntegrator.User.Username
+  //   );
+  // } catch (err) {
+  //   if (err.code === "UsernameExistsException") {
+  //     console.log("Email already exists in Integrator Pool :: ", userPool);
+  //     return formatJSONResponseStatusBadRequest({
+  //       message: "Email already exists.",
+  //     });
+  //   } else {
+  //     console.log(
+  //       "Error while adding Integrator in Integrator Pool(adminCreateUser) :: ",
+  //       err
+  //     );
+  //     return formatJSONResponseStatusServerError({
+  //       message: "Error while adding Integrator in Integrator Pool",
+  //       err,
+  //     });
+  //   }
+  // }
+
+  // try {
+    // await cognitoIdp
+    //   .adminAddUserToGroup({
+    //     GroupName: "IntegratorGroup",
+    //     UserPoolId: userPool,
+    //     Username: email,
+    //   })
+  //     .promise();
+  //   console.log(
+  //     `Integrator with id ${integrator_id} is added to IntegratorGroup`
+  //   );
+  // } catch (err) {
+  //   console.log(
+  //     "Error while adding Integrator in Integrator Pool (adminAddUserToGroup) :: ",
+  //     err
+  //   );
+  //   return formatJSONResponseStatusServerError({
+  //     message: "Error while adding Integrator in Integrator Pool",
+  //     err,
+  //   });
+  // }
 };
